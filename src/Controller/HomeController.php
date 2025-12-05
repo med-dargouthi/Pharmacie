88392ee4d733909b6d8d7386b6c9607a525a9f6d
Skaanzz <167695108+Skaanzz@users.noreply.github.com>
2025-12-05 11:12:47 +0100

    Change render template path in HomeController

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,15 +6,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
     public function index(): Response
     {
         $user = $this->getUser();
-       return $this->render('index.html.twig', ['user' => $this->getUser()
-                            ]);
 
+        return $this->render('home/index.html.twig', [
+            'user' => $user
+        ]);
     }
 }
